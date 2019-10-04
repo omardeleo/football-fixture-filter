c3061eee1762cd3e2c84ae42ef36527bc443222d
Omar De Leo <omar.deleo@gmail.com>
2019-10-03 20:34:55 -0400

    Switch parser to lxml

--- scraper.py
+++ scraper.py
@@ -3,6 +3,6 @@ from bs4 import BeautifulSoup
 
 data_page = 'https://www.espn.com/soccer/standings/_/league/eng.1'
 page = urlopen(data_page)
-soup = BeautifulSoup(page, 'html.parser')
+soup = BeautifulSoup(page, 'lxml')
 table_box = soup.find(name='section', attrs={'class':'ResponsiveTable ResponsiveTable--fixed-left'})
 print(table_box.prettify())
