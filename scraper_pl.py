37e22b15ae268f141e937571a3b68390dcf64539
Omar De Leo <omar.deleo@gmail.com>
2019-10-07 01:12:48 -0400

    Refactor

--- scraper_pl.py
+++ scraper_pl.py
@@ -8,28 +8,30 @@ page = urlopen(data_page)
 soup = BeautifulSoup(page, 'lxml')
 # select element with team info
 team = soup.select_one('td[scope]')
-classes = team['class']
+teamInfo = {}
 teamName = team.getText()
 slice1 = slice(0, -4)
 slice2 = slice(-4, -1)
-print("***")
-print("***")
-print("***")
+teamInfo["name"] = teamName[slice1].strip()
+teamInfo["symbol"] = teamName[slice2].strip()
+
 fields = ["gamesPlayed", "won", 
     "drawn", "lost", "goalsFor", "goalsAgainst", 
     "goalDifference", "points"]
-teamInfo = {}
-teamInfo["name"] = teamName[slice1].strip()
-teamInfo["symbol"] = teamName[slice2].strip()
-# print(teamInfo)
+
 counter = 0
+
+classes = team['class']
 while 'points' not in classes:
     team = team.find_next('td')
     classes = team['class'] if team.has_attr('class') else []
     teamInfo[fields[counter]] = team.getText().strip()
     counter += 1
 
-print(teamInfo)
+# with open('data.txt', 'w') as outfile:
+#     json.dump(teamInfo, outfile)
 
-with open('data.txt', 'w') as outfile:
-    json.dump(teamInfo, outfile)
+print("***")
+print("***")
+print("***")
+print(teamInfo)
