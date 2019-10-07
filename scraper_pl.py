902e6611db834481ca007bf21eafa9ee2d6ef64a
Omar De Leo <omar.deleo@gmail.com>
2019-10-07 01:06:15 -0400

    Generate JSON file

--- scraper_pl.py
+++ scraper_pl.py
@@ -1,18 +1,35 @@
+import json
+
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 
 data_page = 'https://www.premierleague.com/tables'
 page = urlopen(data_page)
 soup = BeautifulSoup(page, 'lxml')
-data_rows = soup.select_one('tr[data-filtered-table-row-name]')
-# soup.select_one('td[scope]')
-# the above selects the td element with the team abbr name
-# the following td's have the table info for that team up until
-# <td class="points">...</td>
-print("")
-print("")
-print("")
-print("")
-print("")
-print("")
-print(data_rows)
+# select element with team info
+team = soup.select_one('td[scope]')
+classes = team['class']
+teamName = team.getText()
+slice1 = slice(0, -4)
+slice2 = slice(-4, -1)
+print("***")
+print("***")
+print("***")
+fields = ["gamesPlayed", "won", 
+    "drawn", "lost", "goalsFor", "goalsAgainst", 
+    "goalDifference", "points"]
+teamInfo = {}
+teamInfo["name"] = teamName[slice1].strip()
+teamInfo["symbol"] = teamName[slice2].strip()
+# print(teamInfo)
+counter = 0
+while 'points' not in classes:
+    team = team.find_next('td')
+    classes = team['class'] if team.has_attr('class') else []
+    teamInfo[fields[counter]] = team.getText().strip()
+    counter += 1
+
+print(teamInfo)
+
+with open('data.txt', 'w') as outfile:
+    json.dump(teamInfo, outfile)
