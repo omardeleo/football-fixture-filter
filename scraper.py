42dd53fa5385009feccd8d2d0f321772f49c9eb0
Omar De Leo <omar.deleo@gmail.com>
2019-10-07 01:10:25 -0400

    Delete old script

--- scraper.py
+++
@@ -1,8 +0,0 @@
-from urllib.request import urlopen
-from bs4 import BeautifulSoup
-
-data_page = 'https://www.espn.com/soccer/standings/_/league/eng.1'
-page = urlopen(data_page)
-soup = BeautifulSoup(page, 'lxml')
-table_box = soup.find(name='section', attrs={'class':'ResponsiveTable ResponsiveTable--fixed-left'})
-print(table_box.prettify())
