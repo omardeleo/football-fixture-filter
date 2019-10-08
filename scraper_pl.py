dec37704738ea58371fe14aa1e15a61cf5145118
Omar De Leo <omar.deleo@gmail.com>
2019-10-08 14:09:12 -0400

    Create createTeamData function

--- scraper_pl.py
+++ scraper_pl.py
@@ -6,32 +6,29 @@ from bs4 import BeautifulSoup
 data_page = 'https://www.premierleague.com/tables'
 page = urlopen(data_page)
 soup = BeautifulSoup(page, 'lxml')
-# select element with team info
 team = soup.select_one('td[scope]')
-teamInfo = {}
-teamName = team.getText()
-slice1 = slice(0, -4)
-slice2 = slice(-4, -1)
-teamInfo["name"] = teamName[slice1].strip()
-teamInfo["symbol"] = teamName[slice2].strip()
 
 fields = ["gamesPlayed", "won", 
     "drawn", "lost", "goalsFor", "goalsAgainst", 
     "goalDifference", "points"]
 
-counter = 0
-
-classes = team['class']
-while 'points' not in classes:
-    team = team.find_next('td')
-    classes = team['class'] if team.has_attr('class') else []
-    teamInfo[fields[counter]] = team.getText().strip()
-    counter += 1
-
-# with open('data.txt', 'w') as outfile:
-#     json.dump(teamInfo, outfile)
+def createTeamData(elem):
+    counter = 0
+    teamInfo = {}
+    teamName = elem.getText()
+    slice1 = slice(0, -4)
+    slice2 = slice(-4, -1)
+    teamInfo["name"] = teamName[slice1].strip()
+    teamInfo["symbol"] = teamName[slice2].strip()
+    classes = elem['class']
+    while 'points' not in classes:
+        elem = elem.find_next('td')
+        classes = elem['class'] if elem.has_attr('class') else []
+        teamInfo[fields[counter]] = elem.getText().strip()
+        counter += 1
+    return teamInfo
 
 print("***")
 print("***")
-print("***")
-print(teamInfo)
+data = createTeamData(team)
+print(data)
